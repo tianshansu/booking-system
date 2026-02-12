b28afeea11e1cf8eed8e738849222b0e111986b9
Tianshan Su <sutianshan2008@163.com>
2026-02-12 14:29:49 +1100

    Update header bar logic
    -dynamically display title and text in header bar according to path
    -fix border-bottom issue

--- frontend/src/components/layout/HeaderBar.jsx
+++ frontend/src/components/layout/HeaderBar.jsx
@@ -1,11 +1,33 @@
+import './HeaderBar.css';
+import { useLocation } from "react-router-dom";
+
+const TITLE_MAP = {
+    "/": "Dashboard",
+    "/people": "People",
+    "/sessions": "Sessions",
+};
+
+const SUBTITLE_MAP = {
+    "/": "Welcome back!",
+    "/people": "Manage people",
+    "/sessions": "Manage sessions",
+};
+
 export default function HeaderBar(){
+    const location = useLocation();
+    const path = location.pathname; //get path name from location
+
+    const title = TITLE_MAP[path] ?? "Dashboard"; //if the path exists in TITLE_MAP, then give the value to title
+    const subtitle = SUBTITLE_MAP[path] ?? "Welcome back!"; //similar to above
+
     return (
-        <header style={{width:"100%", display:"flex", justifyContent:"space-between"}}>
-            <div>
-                <div style={{fontWeight:800, fontSize:20}}>Dashboard</div>
-                <div style={{fontSize:14}}>Welcome back!</div>
+        <header className='header'>
+            <div className='header-content'>
+                {/* display values */}
+                <div className='header-content-title'>{title}</div> 
+                <div className='header-content-text'>{subtitle}</div>
             </div>
-            <input placeholder="Search..." style={{width:260}}></input>
+            <input className='header-search' placeholder="Search..." ></input>
         </header>
     );
 }
