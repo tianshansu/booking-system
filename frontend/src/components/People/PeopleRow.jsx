609f611aef1845dafe46c0d0724a29ddce077d6f
Tianshan Su <sutianshan2008@163.com>
2026-02-22 11:38:28 +1100

    Update people page & mock data
    - extract mock data from dashboard and create a noew folder to place
      mock data
    - people page structure done (table)
    - add a new extension for code layout manangement

--- frontend/src/components/People/PeopleRow.jsx
+++ frontend/src/components/People/PeopleRow.jsx
@@ -1,34 +1,45 @@
 import "./PeopleRow.css";
 
-export default function PeopleRow({person}){
-    return(
-        <tr className="people-row">
-            <td className="people-row-cell people-row-checkbox">
-                <input type="checkbox"></input>
-            </td>
-            <td className="people-row-cell people-row-name">
-                <p>{person.name}</p>
-            </td>
-            <td className="people-row-cell people-row-email">
-                <p>{person.email}</p>
-            </td>
-            <td className="people-row-cell people-row-phone">
-                <p>{person.phone}</p>
-            </td>
-            <td className="people-row-cell people-row-status">
-                <p>{person.status}</p>
-            </td>
-            <td className="people-row-cell people-row-last-session">
-                <p>{person.lastSession}</p>
-            </td>
-            <td className="people-row-cell people-row-notes">
-                <p>{person.notes}</p>
-            </td>
-            <td className="people-row-cell people-row-actions">
-                <button type="button">
-                    <img src="/icons/more.svg" alt="more info"></img>
-                </button>
-            </td>
-        </tr>
-    )
+export default function PeopleRow({ person }) {
+  return (
+    <tr className="people-row">
+      <td className="people-row-cell people-row-checkbox">
+        <input type="checkbox"></input>
+      </td>
+      <td className="people-row-cell people-row-name">
+        <p>{person.name}</p>
+      </td>
+      <td className="people-row-cell people-row-email">
+        <p>{person.email}</p>
+      </td>
+      <td className="people-row-cell people-row-phone">
+        <p>{person.phone}</p>
+      </td>
+      <td className="people-row-cell people-row-status">
+        <p>{person.status}</p>
+      </td>
+      <td className="people-row-cell people-row-last-session">
+        <p>{person.lastSession}</p>
+      </td>
+      <td className="people-row-cell people-row-notes">
+        <p>{person.notes}</p>
+      </td>
+      <td className="people-row-cell people-row-actions">
+        <button
+          type="button"
+          className="people-row-action-button"
+          style={{ color: "#4338CA" }}
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          className="people-row-action-button"
+          style={{ color: "#DC2626" }}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
 }
