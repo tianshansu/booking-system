b8fc4fdf2ca475156205710ec33ee2e148f141d0
Tianshan Su <sutianshan2008@163.com>
2026-05-07 14:55:42 +1000

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,24 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
 const peopleRoutes = require("./routes/peopleRoutes");
 const sessionsRoutes = require("./routes/sessionsRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const authRoutes = require("./routes/authRoutes");
-const authMiddleware = require("./middleware/authMiddleware"); //check login
+const authMiddleware = require("./middleware/authMiddleware"); // check login
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.get("/health", (req, res) => {
@@ -22,6 +32,8 @@ app.use("/api/dashboard", authMiddleware, dashboardRoutes);
 
 app.use("/api/auth", authRoutes);
 
-app.listen(4000, () => {
-  console.log("Backend running: http://localhost:4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
